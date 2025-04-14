ac035d631b3da0b2f1dca48ed492aea750227899
cracker38 <it.elias38@gmail.com>
2025-04-14 23:26:43 +0200

    middleware are updated successful

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 class AdminMiddleware
 {
     /**
-     * Handle an incoming request.
+     * Handle an incoming reques.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
